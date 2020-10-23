925cc59333f9b0b294c8b0da2906563b7c79c9f0
yashrjt <esharaagav@outlook.com>
2020-10-23 17:35:15 -0400

    Forms completed

--- Javascript/forms.js
+++ Javascript/forms.js
@@ -1,4 +1,41 @@
+$.validator.addMethod('alphabets',function(value){
+return /^[A-Za-z]+$/.test(value);
+})
+
+
 $('#accountForm').validate({
+    rules:{
+        name:{
+            required:true,
+            minlength:2,
+            alphabets:true
+        },
+        email:{
+            required:true
+        },
+        pwd:{
+            required:true
+        },
+        cpwd:{
+            required:true
+        }
+    },
+    messages:{
+        name:{
+            required:'Name is a required field',
+            minlength:'Name must be minimum 2 characters',
+            alphabets:'Please enter only alphabets for name'
+        },
+        email:{
+            required:'Email is a required field',
+        },
+        pwd:{
+            required:'Password is a required field',
+        },
+        cpwd:{
+            required:'Confirm password is a required field',
+        }
+    },
     submitHandler:function(){
 
 
